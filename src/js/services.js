07089240762e1db01b48279f172f10448c7aec95
Adam Franzen <adamfranzen@gmail.com>
2016-10-24 15:26:30 -0600

    pushing for gh-pages deployment

--- src/js/services.js
+++ src/js/services.js
@@ -10,15 +10,19 @@
 
     function coffeeService($http) {
       this.getAllCoffee = function() {
-        return $http.get('https://enigmatic-gorge-65304.herokuapp.com/coffee');
+        return $http.get('http://localhost:8000/coffee');
       };
 
       this.getSingleCoffee = function(id) {
-        return $http.get(`https://enigmatic-gorge-65304.herokuapp.com/coffee/${id}`);
+        return $http.get(`http://localhost:8000/coffee/${id}`);
       }
 
       this.addCoffee = function(coffee) {
-        return $http.post('https://enigmatic-gorge-65304.herokuapp.com/coffee', coffee);
+        return $http.post({
+          method: 'POST',
+          url: 'http://localhost:8000/coffee',
+          data: coffee,
+          headers: {}});
       }
     }
 
