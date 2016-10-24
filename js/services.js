b91b2d5e7331790202695a7f143791e18f01c880
Adam Franzen <adamfranzen@gmail.com>
2016-10-24 13:21:39 -0600

    cross domain request

--- js/services.js
+++ js/services.js
@@ -6,8 +6,13 @@
     .module('myApp.services', [])
     .service('coffeeService', coffeeService);
 
-    function coffeeService() {
-      this.test = 'does this work?'
+  coffeeService.$inject = ['$http']
+
+    function coffeeService($http) {
+      this.getAllCoffee = function() {
+        return $http.get('https://enigmatic-gorge-65304.herokuapp.com/coffee');
+      };
+        this.test = 'does this work?'
     }
 
 })();
